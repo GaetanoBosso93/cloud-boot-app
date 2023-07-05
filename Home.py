ee5dcb9ad8be8d95b7ecc32d4f2622fad39d4097
Gaetano Bosso <gaetanobosso1993@gmail.com>
2023-07-05 13:11:15 +0200

    prima call ai dati

--- Home.py
+++ Home.py
@@ -1,3 +1,8 @@
+import pandas as pd
 import streamlit as st
 
 st.title("Hello everyone!")
+
+df = pd.read_csv("https://storage.googleapis.com/cloud-boot-app-bucket/1553768847-housing.csv")
+
+st.table(df)
